7f2aa8c9db445e352b5d86bcee519b069d52d170
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-08-13 12:30:00 +0530

    Initial Commit
    This is the initial commit of the Group Project web site's files.

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Home</title>
+    <link rel="shortcut icon" type="image/png" href="https://www.niwder.me/tvdb/logo.jpg"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #212529;">
+        <a class="navbar-brand" href="./">Regional Hospital, Bentota</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="./">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./#">Link1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./#">Link2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link3</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./#">Link4</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
