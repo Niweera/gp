47a8e1c5c2b43759d29707bc478d5020e4bda1ab
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-09-07 19:38:13 +0530

    Create about page

---
+++ about/index.php
@@ -0,0 +1,37 @@
+<?php
+	include '../header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link rel="stylesheet" type="text/css" href="../styles.css">
+</head>
+<body>
+
+
+	</div>
+	</div>
+	<div class="head-bar">
+		<div class="image1">
+			<img src="../sourcefiles/1.jpg" alt="about">
+		</div><!--image1-->
+	</div><!--head-bar-->
+	<div class="column">
+		<div class="column-1">
+			<h2>History</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div><!--column-1-->
+		<div class="column-1">
+			<h2>History</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div><!--column-1-->
+		<div class="column-2">
+		</div><!--column-2-->
+	</div><!--column-->
+
+</body>
+<?php
+	include '../footer.php';
+?>
+</html>
