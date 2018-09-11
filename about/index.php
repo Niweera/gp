fbe84b4f137e1589b70443050df1cf4bebd3a61a
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-09-11 23:27:59 +0530

    Update about page

--- about/index.php
+++ about/index.php
@@ -112,6 +112,48 @@
 		</div><!--column-2-->
 	</div><!--column-->
 <br>
+<div class="column">
+        <div class="column-1">
+            <h2>History</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div><!--column-1-->
+        <div class="column-1">
+            <h2>History</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div><!--column-1-->
+        <div class="column-2">
+            <h2>Responsive Image Gallery</h2>
+            <div class="responsive">
+             <div class="gallery">
+                <a target="_blank" href="img_5terre.jpg">
+                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+                </a>
+            </div>
+            </div>
+            <div class="responsive">
+             <div class="gallery">
+                <a target="_blank" href="img_5terre.jpg">
+                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+                </a>
+            </div>
+            </div>
+            <div class="responsive">
+             <div class="gallery">
+                <a target="_blank" href="img_5terre.jpg">
+                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+                </a>
+            </div>
+            </div>
+            <div class="responsive">
+             <div class="gallery">
+                <a target="_blank" href="img_5terre.jpg">
+                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+                </a>
+            </div>
+            </div>
+        </div><!--column-2-->
+    </div><!--column-->
+<br>
 <?php
 	include '../footer.php';
 ?>
