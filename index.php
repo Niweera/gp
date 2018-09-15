a219b8b934b3d39b1f9e57f4db89f313356ee681
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-09-15 14:53:10 +0530

    Update home page

--- index.php
+++ index.php
@@ -220,13 +220,7 @@
 
      <!-- GOOGLE MAP -->
      <section id="google-map">
-     <!-- How to change your own map point
-            1. Go to Google Maps
-            2. Click on your location point
-            3. Click "Share" and choose "Embed map" tab
-            4. Copy only URL and paste it within the src="" field below
-     -->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.7700428822654!2d79.99731371431461!3d6.42358499535247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae22e9a563588b5%3A0xdf5ab81622564e9b!2sGovernment+Hospital+Bentota!5e0!3m2!1sen!2slk!4v1537003110727" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>  
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.7700428822654!2d79.99731371431461!3d6.42358499535247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae22e9a563588b5%3A0xdf5ab81622564e9b!2sGovernment+Hospital+Bentota!5e0!3m2!1sen!2slk!4v1537003110727" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>  
      </section>           
 
 
