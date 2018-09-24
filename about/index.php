561c4f8c8e36a60c299aa682128e24ba68a4de0c
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-09-24 22:09:15 +0530

    Update home page

--- about/index.php
+++ about/index.php
@@ -2,163 +2,133 @@
 	session_start();
     include '../dbconf/dbh.php';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
+
     <title>Home</title>
+
     <link rel="shortcut icon" type="image/png" href="https://www.niwder.me/tvdb/logo.jpg"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="../css/bootstrap.min.css">
+     <link rel="stylesheet" href="../css/font-awesome.min.css">
+     <link rel="stylesheet" href="../css/animate.css">
+     <link rel="stylesheet" href="../css/owl.carousel.css">
+     <link rel="stylesheet" href="../css/owl.theme.default.min.css">
 
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/animate.css">
-    <link rel="stylesheet" href="../css/owl.carousel.css">
-    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
-
+    
     <!--MAIN-->
     <link rel="stylesheet" href="../styles.css"/>
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../css/tooplate-style.css">
    
 
 
 
 </head>
 <body>
-<!--Header navigation bar for the website-->
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #212529;">
-    <a class="navbar-brand" href="./">Divisional Hospital, Bentota</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="<?php if (isset($_SESSION['userid'])) {include './homelink.php';}else{echo "../";}?>"><?php if (isset($_SESSION['userid'])) { include '../homename.php';}else{echo "";}?> Home<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            About Us
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item active" href="./about">Overview</a>
-                <a class="dropdown-item" href="#">Vision & Mission</a>
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Services
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Clinic Services</a>
-                <a class="dropdown-item" href="#">OPD Services</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./#">Contact Details</a>
-        </li>
-        <li class="nav-item">
-            <?php
-                if(!isset($_SESSION['userid'])){
-                    echo '<a class="nav-link" href="../login">Log in</a>';
-                }else{
-                    echo '<a class="nav-link" href="../logout">Log out</a>';
-                }
-            ?>
-        </li>
-        
-        </ul>
-    </div>
-</nav>
-<!--End of the Header navigation bar for the website-->
-<br>
- <!-- HOME -->
-     <section id="home" class="slider" data-stellar-background-ratio="0.5">
+ <!-- MENU -->
+     <section class="navbar navbar-default navbar-static-top" role="navigation">
           <div class="container">
-               <div class="row">
 
-                         <div class="owl-carousel owl-theme">
-                              <div class="item item-first">
-                                   <div class="caption">
-                                        <div class="col-md-offset-1 col-md-10">
-                                             <h1>Your Hospital</h1>
-                                        </div>
-                                   </div>
-                              </div>
-
-                              <div class="item item-second">
-                                   <div class="caption">
-                                        <div class="col-md-offset-1 col-md-10">
-                                             <h1>New Lifestyle</h1>
-                                        </div>
-                                   </div>
-                              </div>
-
-                              <div class="item item-third">
-                                   <div class="caption">
-                                        <div class="col-md-offset-1 col-md-10">
-                                             <h1>Healthy Living</h1>
-                                        </div>
-                                   </div>
-                              </div>
-                         </div>
-
+               <div class="navbar-header">
+                   
+                    <!-- lOGO TEXT HERE -->
+                    <a href="../" class="navbar-brand"><i class="fa fa-h-square"></i>&nbsp &nbsp Divisional Hospital, Bentota</a>
                </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                         <li class="nav-item">
+                            <a class="nav-link" href="<?php if (isset($_SESSION['userid'])) {include '../homelink.php';}else{echo "../";}?>"><?php if (isset($_SESSION['userid'])) { include '../homename.php';}else{echo "";}?> Home<span class="sr-only">(current)</span></a>
+                        </li>
+
+                         <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="./about">Overview</a></li>
+                                    <li><a href="index-video.html">Vission & Mission</a></li>
+                                </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="./about">Clinic Services</a></li>
+                                    <li><a href="index-video.html">OPD Services</a></li>
+                                </ul>
+                        </li>
+                         
+                         <li><a href="#" class="smoothScroll">Contact Details</a></li>
+                         <li class="nav-item">
+                            <?php
+                                if(!isset($_SESSION['userid'])){
+                                    echo '<a class="nav-link" href="../login">Log in</a>';
+                                }else{
+                                    echo '<a class="nav-link" href="../logout">Log out</a>';
+                                        }
+                            ?>
+                        </li>
+                    </ul>
+               </div>
+
           </div>
      </section>
+<br>
+ <!-- HOME -->
+  <div class="head-image">
+    <img src="../images/14.jpeg" alt="about">
+    <h1>Healthy Living</h1>
+  </div>
 
-	<div class="column">
-		<div class="column-1">
-			<h2>History</h2>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>
+  <div class="column">
+    <div class="column-1">
+      <h2>History</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
-		</div><!--column-1-->
-		<div class="column-1">
-			<h2>History</h2>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>
+    </div><!--column-1-->
+    <div class="column-1">
+      <h2>History</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
         </p>
-		</div><!--column-1-->
-		<div class="column-2">
-			<h2>Responsive Image Gallery</h2>
-			<div class="responsive">
- 			 <div class="gallery">
-    			<a target="_blank" href="../images/1.jpeg">
-      			<img src="../images/1.jpeg" alt="Cinque Terre" width="600" height="400">
-    			</a>
-  			</div>
-			</div>
-			<div class="responsive">
- 			 <div class="gallery">
-    			<a target="_blank" href="../images/2.jpeg">
-      			<img src="../images/2.jpeg" alt="Cinque Terre" width="600" height="400">
-    			</a>
-  			</div>
-			</div>
-			<div class="responsive">
- 			 <div class="gallery">
-    			<a target="_blank" href="../images/3.jpeg">
-      			<img src="../images/3.jpeg" alt="Cinque Terre" width="600" height="400">
-    			</a>
-  			</div>
-			</div>
-			<div class="responsive">
- 			 <div class="gallery">
-    			<a target="_blank" href="../images/4.jpeg">
-      			<img src="../images/4.jpeg" alt="Cinque Terre" width="600" height="400">
-    			</a>
-  			</div>
-			</div>
+    </div><!--column-1-->
+    <div class="column-2">
+      <h2>Responsive Image Gallery</h2>
+      <div class="responsive">
+       <div class="gallery">
+          <a target="_blank" href="../images/1.jpeg">
+            <img src="../images/1.jpeg" alt="Cinque Terre" width="600" height="400">
+          </a>
+        </div>
+      </div>
+      <div class="responsive">
+       <div class="gallery">
+          <a target="_blank" href="../images/2.jpeg">
+            <img src="../images/2.jpeg" alt="Cinque Terre" width="600" height="400">
+          </a>
+        </div>
+      </div>
+      <div class="responsive">
+       <div class="gallery">
+          <a target="_blank" href="../images/3.jpeg">
+            <img src="../images/3.jpeg" alt="Cinque Terre" width="600" height="400">
+          </a>
+        </div>
+      </div>
+      <div class="responsive">
+       <div class="gallery">
+          <a target="_blank" href="../images/4.jpeg">
+            <img src="../images/4.jpeg" alt="Cinque Terre" width="600" height="400">
+          </a>
+        </div>
+      </div>
 
             <div class="responsive">
              <div class="gallery">
@@ -306,9 +276,75 @@
             </div>
 
 
-		</div><!--column-2-->
-	</div><!--column-->
-<br>
+    </div><!--column-2-->
+  </div><!--column-->
+
+   <!-- FOOTER -->
+     <footer data-stellar-background-ratio="5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="footer-thumb"> 
+                              <h4 class="wow fadeInUp" data-wow-delay="0.4s">Contact Info</h4>
+                              <p>Fusce at libero iaculis, venenatis augue quis, pharetra lorem. Curabitur ut dolor eu elit consequat ultricies.</p>
+
+                              <div class="contact-info">
+                                   <p><i class="fa fa-phone"></i> 010-070-0170</p>
+                                   <p><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4"> 
+                         <div class="footer-thumb">
+                              <div class="opening-hours">
+                                   <h4 class="wow fadeInUp" data-wow-delay="0.4s">Follow Us</h4>
+                              </div> 
+                              <ul class="social-icon">
+                                   <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                                   <li><a href="#" class="fa fa-twitter"></a></li>
+                                   <li><a href="#" class="fa fa-instagram"></a></li>
+                                   <li><a href="#" class="fa fa-google-plus"></a></li>
+                              </ul>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4"> 
+                         <div class="footer-thumb"> 
+                              <h4 class="wow fadeInUp" data-wow-delay="0.4s">Our Location</h4>
+                              <div class="latest-stories">
+                                   <div class="stories-image">
+                                        <a href="#"><img src="../images/news-image.jpg" class="img-responsive" alt=""></a>
+                                   </div>
+                                   <div class="stories-info">
+                                        <h5>Address</h5></a>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 border-top">
+                         <div class="col-md-4 col-sm-6">
+                              <div class="copyright-text"> 
+                                   <p>Copyright &copy; 2018 Divisional Hospital of Bentota. | All right Reserved.</p>
+                              </div>
+                         </div>
+                         <div class="col-md-6 col-sm-6">
+                            <div class="copyright-text"> 
+                               <p>Developed by Group 16 of University of Colombo School of Computing</p>
+                             </div>
+                         </div>
+                         <div class="col-md-2 col-sm-2 text-align-center">
+                              <div class="angle-up-btn"> 
+                                  <a href="#top" class="smoothScroll wow fadeInUp" data-wow-delay="1.2s"><i class="fa fa-angle-up"></i></a>
+                              </div>
+                         </div>   
+                    </div>
+               </div>
+          </div>
+     </footer>        
+
 
  <!-- SCRIPTS -->
      <script src="../js/jquery.js"></script>
@@ -320,7 +356,6 @@
      <script src="../js/owl.carousel.min.js"></script>
      <script src="../js/custom.js"></script>
 
-<br>
-<?php
-	include '../footer.php';
-?>
+</body>
+</html>
+
