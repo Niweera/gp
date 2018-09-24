4819ec55e72d135cdd5e45aed8ee776a7a1e57f6
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-09-24 22:10:39 +0530

    Update login page

--- login/index.php
+++ login/index.php
@@ -76,56 +76,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Home</title>
+    <title>Login</title>
     <link rel="shortcut icon" type="image/png" href="https://www.niwder.me/tvdb/logo.jpg"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    
     <link rel="stylesheet" href="../styles.css"/>
-	<link rel="stylesheet" href="./style.css"/>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css"/>
+    <link rel="stylesheet" href="../css/tooplate-style.css">
+    
 </head>
 <body id="LoginForm">
-<!--Header navigation bar for the website-->
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #212529;">
-    <a class="navbar-brand" href="../">Divisional Hospital, Bentota</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="../">Home<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            About Us
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="../about">Overview</a>
-                <a class="dropdown-item" href="#">Vision & Mission</a>
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Services
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Clinic Services</a>
-                <a class="dropdown-item" href="#">OPD Services</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./#">Contact Details</a>
-        </li>
-        <li class="nav-item">
-            
-            <a class="nav-link active" href="./">Login</a>
-        </li>
-        </ul>
-    </div>
-</nav>
-<!--End of the Header navigation bar for the website-->
+<!-- MENU -->
+     <section class="navbar navbar-default navbar-static-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                   
+                    <!-- lOGO TEXT HERE -->
+                    <a href="../" class="navbar-brand"><i class="fa fa-h-square"></i>&nbsp &nbsp Divisional Hospital, Bentota</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                         <li class="nav-item">
+                            <a class="nav-link" href="<?php if (isset($_SESSION['userid'])) {include '../homelink.php';}else{echo "../";}?>"><?php if (isset($_SESSION['userid'])) { include '../homename.php';}else{echo "";}?> Home<span class="sr-only">(current)</span></a>
+                        </li>
+
+                         <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="../about">Overview</a></li>
+                                    <li><a href="index-video.html">Vission & Mission</a></li>
+                                </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="./about">Clinic Services</a></li>
+                                    <li><a href="index-video.html">OPD Services</a></li>
+                                </ul>
+                        </li>
+                         
+                         <li><a href="#" class="smoothScroll">Contact Details</a></li>
+                         <li class="nav-item">
+                            <?php
+                                if(!isset($_SESSION['userid'])){
+                                    echo '<a class="nav-link" href="./login">Log in</a>';
+                                }else{
+                                    echo '<a class="nav-link" href="./logout">Log out</a>';
+                                        }
+                            ?>
+                        </li>
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
 <!-- login modal codes-->
 <div class="container">
     <div class="login-form">
@@ -159,6 +173,84 @@
 </div>
 </div>
 
-	<?php
-		include '../footer.php';
-	?>
+   <!-- FOOTER -->
+     <footer data-stellar-background-ratio="5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="footer-thumb"> 
+                              <h4 class="wow fadeInUp" data-wow-delay="0.4s">Contact Info</h4>
+                              <p>Fusce at libero iaculis, venenatis augue quis, pharetra lorem. Curabitur ut dolor eu elit consequat ultricies.</p>
+
+                              <div class="contact-info">
+                                   <p><i class="fa fa-phone"></i> 010-070-0170</p>
+                                   <p><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></p>
+                              </div>
+                         </div>
+                    </div>
+
+                     <div class="col-md-4 col-sm-4"> 
+                         <div class="footer-thumb">
+                              <div class="opening-hours">
+                                   <h4 class="wow fadeInUp" data-wow-delay="0.4s">Follow Us</h4>
+                              </div> 
+                              <ul class="social-icon">
+                                   <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                                   <li><a href="#" class="fa fa-twitter"></a></li>
+                                   <li><a href="#" class="fa fa-instagram"></a></li>
+                                   <li><a href="#" class="fa fa-google-plus"></a></li>
+                              </ul>
+                         </div>
+                    </div>
+
+
+                    <div class="col-md-4 col-sm-4"> 
+                         <div class="footer-thumb"> 
+                              <h4 class="wow fadeInUp" data-wow-delay="0.4s">Our Location</h4>
+                              <div class="latest-stories">
+                                   <div class="stories-image">
+                                        <a href="#"><img src="../images/news-image.jpg" class="img-responsive" alt=""></a>
+                                   </div>
+                                   <div class="stories-info">
+                                        <h5>Address</h5></a>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 border-top">
+                         <div class="col-md-4 col-sm-6">
+                              <div class="copyright-text"> 
+                                   <p>Copyright &copy; 2018 Divisional Hospital of Bentota. | All right Reserved.</p>
+                              </div>
+                         </div>
+                         <div class="col-md-6 col-sm-6">
+                            <div class="copyright-text"> 
+                               <p>Developed by Group 16 of University of Colombo School of Computing</p>
+                             </div>
+                         </div>
+                         <div class="col-md-2 col-sm-2 text-align-center">
+                              <div class="angle-up-btn"> 
+                                  <a href="#top" class="smoothScroll wow fadeInUp" data-wow-delay="1.2s"><i class="fa fa-angle-up"></i></a>
+                              </div>
+                         </div>   
+                    </div>
+               </div>
+          </div>
+     </footer>        
+
+
+ <!-- SCRIPTS -->
+     <script src="../js/jquery.js"></script>
+     <script src="../js/bootstrap.min.js"></script>
+     <script src="../js/jquery.sticky.js"></script>
+     <script src="../js/jquery.stellar.min.js"></script>
+     <script src="../js/wow.min.js"></script>
+     <script src="../js/smoothscroll.js"></script>
+     <script src="../js/owl.carousel.min.js"></script>
+     <script src="../js/custom.js"></script>
+
+</body>
+</html>
+
