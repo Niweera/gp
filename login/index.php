c1883a2813762de2205cf7f912cbd3f100038dd6
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-09-27 17:45:34 +0530

    Update login page

--- login/index.php
+++ login/index.php
@@ -1,25 +1,25 @@
 <?php
-	include '../dbconf/dbh.php';
-	session_start();
-	
-	
-	if(isset($_POST['submit']))
-	{
-		if((isset($_POST['email']) && $_POST['email'] !='') && (isset($_POST['password']) && $_POST['password'] !=''))
-		{
-			$email = trim(mysqli_real_escape_string($conn, $_POST['email']));
-			$password = trim(mysqli_real_escape_string($conn, $_POST['password']));
-			
-			$sqlEmail = "select * from user where userid = '".$email."'";
-			$rs = mysqli_query($conn,$sqlEmail);
-			$numRows = mysqli_num_rows($rs);
-			
-			if($numRows  == 1)
-			{
-				$row = mysqli_fetch_assoc($rs);
-				
-				if(password_verify($password,$row['password']))
-				{
+  include '../dbconf/dbh.php';
+  session_start();
+  
+  
+  if(isset($_POST['submit']))
+  {
+    if((isset($_POST['email']) && $_POST['email'] !='') && (isset($_POST['password']) && $_POST['password'] !=''))
+    {
+      $email = trim(mysqli_real_escape_string($conn, $_POST['email']));
+      $password = trim(mysqli_real_escape_string($conn, $_POST['password']));
+      
+      $sqlEmail = "select * from user where userid = '".$email."'";
+      $rs = mysqli_query($conn,$sqlEmail);
+      $numRows = mysqli_num_rows($rs);
+      
+      if($numRows  == 1)
+      {
+        $row = mysqli_fetch_assoc($rs);
+        
+        if(password_verify($password,$row['password']))
+        {
                     session_start();
                     include './function.php';
                     $list = returnFlag($email);
@@ -29,45 +29,45 @@
                     $_SESSION['name'] = $row1['name'];
                     $_SESSION['email'] = $row1['email'];
                     $_SESSION['contactno'] = $row1['contactno'];
-					$_SESSION['userid'] = $row['userid'];
+          $_SESSION['userid'] = $row['userid'];
                     $_SESSION['flag'] = $row['flag'];
                     if ($_SESSION['flag'] == 0){
                         header('location: ../user/admin');
-					    exit;
+              exit;
                     }
                     elseif ($_SESSION['flag'] == 1){
                         header('location: ../user/doctor');
-					    exit;
+              exit;
                     }
-					elseif ($_SESSION['flag'] == 2){
+          elseif ($_SESSION['flag'] == 2){
                         header('location: ../user/dispenser');
-					    exit;
+              exit;
                     }
                     elseif ($_SESSION['flag'] == 3){
                         header('location: ../user/pharmacist');
-					    exit;
+              exit;
                     }
                     elseif ($_SESSION['flag'] == 4){
                         header('location: ../user/nurse');
-					    exit;
+              exit;
                     }
                     elseif ($_SESSION['flag'] == 5){
                         header('location: ../user/patient');
-					    exit;
+              exit;
                     }
-					
-				}
-				else
-				{
-					echo "<script>alert(\"Wrong User ID Or Password!\");</script>";
-				}
-			}
-			else
-			{
-				echo "<script>alert(\"No User Found!\");</script>"; 
-			}
-		}
-	}
+          
+        }
+        else
+        {
+          echo "<script>alert(\"Wrong User ID Or Password!\");</script>";
+        }
+      }
+      else
+      {
+        echo "<script>alert(\"No User Found!\");</script>"; 
+      }
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -76,7 +76,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
+
     <title>Login</title>
+    
     <link rel="shortcut icon" type="image/png" href="https://www.niwder.me/tvdb/logo.jpg"/>
 
     <link rel="stylesheet" href="../css/bootstrap.min.css">
@@ -84,43 +86,46 @@
     <link rel="stylesheet" href="../css/animate.css">
     <link rel="stylesheet" href="../css/owl.carousel.css">
     <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     
     <link rel="stylesheet" href="../styles.css"/>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../css/tooplate-style.css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/tooplate-style.css">    
 </head>
+
 <body id="LoginForm">
 
-<!-- MENU -->
-     <section class="navbar navbar-default navbar-static-top" role="navigation">
-          <div class="container">
+    <!-- MENU -->
+    <section class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container">
 
-               <div class="navbar-header">
-                  <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                    </button>
+            <div class="navbar-header">
+                <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                      <span class="icon icon-bar"></span>
+                      <span class="icon icon-bar"></span>
+                      <span class="icon icon-bar"></span>
+                </button>
                    
-                    <!-- lOGO TEXT HERE -->
-                    <a href="../" class="navbar-brand"><i class="fa fa-h-square"></i>&nbsp &nbsp Divisional Hospital, Bentota</a>
-               </div>
+                <!-- lOGO TEXT HERE -->
+                <a href="../" class="navbar-brand"><i class="fa fa-h-square"></i>&nbsp &nbsp Divisional Hospital, Bentota</a>
+            </div><!--navbar-header-->
 
-               <!-- MENU LINKS -->
-               <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                         <li class="nav-item">
+            <!-- MENU LINKS -->
+            <div class="collapse navbar-collapse">
+                  <ul class="nav navbar-nav navbar-right">
+
+                        <li class="nav-item">
                             <a class="nav-link" href="<?php if (isset($_SESSION['userid'])) {include '../homelink.php';}else{echo "../";}?>"><?php if (isset($_SESSION['userid'])) { include '../homename.php';}else{echo "";}?> Home<span class="sr-only">(current)</span></a>
                         </li>
 
-                         <li class="dropdown">
+                        <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="../about">Overview</a></li>
                                     <li><a href="../about/vission.php">Vission & Mission</a></li>
                                 </ul>
                         </li>
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
                                 <ul class="dropdown-menu">
@@ -129,8 +134,9 @@
                                 </ul>
                         </li>
                          
-                         <li><a href="../contact" class="smoothScroll">Contact Details</a></li>
-                         <li class="nav-item">
+                        <li><a href="../contact" class="smoothScroll">Contact Details</a></li>
+
+                        <li class="nav-item">
                             <?php
                                 if(!isset($_SESSION['userid'])){
                                     echo '<a class="nav-link" href="./login">Log in</a>';
@@ -139,43 +145,47 @@
                                         }
                             ?>
                         </li>
+
                     </ul>
-               </div>
+               </div><!--collapse navbar-collapse-->
 
-          </div>
-     </section>
+          </div><!--container-->
+     </section><!--navbar navbar-default navbar-static-top-->
 
-<!-- login modal codes--><br><br>
-<div class="container bg-danger">
-    <div class="login-form">
+    <!--LOGIN MODAL CODES--><br><br>
+    <div class="container bg-danger">
+      <div class="login-form">
         <div class="main-div">
             <div class="panel">
                 <h2>User Login</h2>
                 <p>Please enter your Login ID and Password</p>
-            </div>
-            <form id="Login" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+            </div><!--panel-->
 
+            <form id="Login" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
                 <div class="form-group">
                     <input type="text" class="form-control" id="inputEmail" placeholder="Login ID" name="email" autocomplete="off" required autofocus>
-                </div>
+                </div><!--form-group-->
 
                 <div class="form-group">
                     <input type="password" class="form-control" id="inputPassword" placeholder="Password" name="password" required autofocus>
-                </div>
+                </div><!--form-group-->
+
                 <div class="forgot">
-                <a href="reset.php">Forgot password?</a>
-                </div>
+                    <a href="reset.php">Forgot password?</a>
+                </div><!--forgot-->
+
                 <button type="submit" class="btn btn-primary" name="submit">Login</button>
 
             </form>
-        </div>
-    </div>
-</div>
 
-   <!-- FOOTER -->
-     <footer data-stellar-background-ratio="5">
+        </div><!--main-div-->
+      </div><!--login-form-->
+  </div><!--container bg-danger-->
+
+      <!-- FOOTER -->
+      <footer data-stellar-background-ratio="5">
           <div class="container">
-               <div class="row">
+              <div class="row">
 
                     <div class="col-md-4 col-sm-4">
                          <div class="footer-thumb"> 
@@ -185,23 +195,23 @@
                               <div class="contact-info">
                                    <p><i class="fa fa-phone"></i> 010-070-0170</p>
                                    <p><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></p>
-                              </div>
-                         </div>
-                    </div>
+                              </div><!--contact-info-->
+                         </div><!--footer-thumb-->
+                    </div><!--col-md-4 col-sm-4-->
 
                      <div class="col-md-4 col-sm-4"> 
                          <div class="footer-thumb">
-                              <div class="opening-hours">
+                              <div class="follow-us">
                                    <h4 class="wow fadeInUp" data-wow-delay="0.4s">Follow Us</h4>
-                              </div> 
+                              </div><!--follow-us-->
                               <ul class="social-icon">
                                    <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
                                    <li><a href="#" class="fa fa-twitter"></a></li>
                                    <li><a href="#" class="fa fa-instagram"></a></li>
                                    <li><a href="#" class="fa fa-google-plus"></a></li>
                               </ul>
-                         </div>
-                    </div>
+                         </div><!--footer-thumb-->
+                    </div><!--col-md-4 col-sm-4-->
 
 
                     <div class="col-md-4 col-sm-4"> 
@@ -210,45 +220,46 @@
                               <div class="latest-stories">
                                    <div class="stories-image">
                                         <a href="#"><img src="../images/news-image.jpg" class="img-responsive" alt=""></a>
-                                   </div>
+                                   </div><!--stories-image-->
                                    <div class="stories-info">
                                         <h5>Address</h5></a>
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
+                                   </div><!--stories-info-->
+                              </div><!--latest-stories-->
+                         </div><!--footer-thumb-->
+                    </div><!--col-md-4 col-sm-4-->
 
                     <div class="col-md-12 col-sm-12 border-top">
                          <div class="col-md-4 col-sm-6">
                               <div class="copyright-text"> 
                                    <p>Copyright &copy; 2018 Divisional Hospital of Bentota. | All right Reserved.</p>
-                              </div>
-                         </div>
+                              </div><!--copyright-text-->
+                         </div><!--col-md-4 col-sm-6-->
                          <div class="col-md-6 col-sm-6">
                             <div class="copyright-text"> 
                                <p>Developed by Group 16 of University of Colombo School of Computing</p>
-                             </div>
-                         </div>
+                            </div><!--copyright-text-->
+                         </div><!--col-md-6 col-sm-6-->
                          <div class="col-md-2 col-sm-2 text-align-center">
                               <div class="angle-up-btn"> 
                                   <a href="#top" class="smoothScroll wow fadeInUp" data-wow-delay="1.2s"><i class="fa fa-angle-up"></i></a>
-                              </div>
-                         </div>   
-                    </div>
-               </div>
-          </div>
+                              </div><!--angle-up-btn-->
+                         </div><!--col-md-2 col-sm-2 text-align-center-->  
+                    </div><!--col-md-12 col-sm-12 border-top-->
+
+               </div><!--row-->
+          </div><!--container-->
      </footer>        
 
 
- <!-- SCRIPTS -->
-     <script src="../js/jquery.js"></script>
-     <script src="../js/bootstrap.min.js"></script>
-     <script src="../js/jquery.sticky.js"></script>
-     <script src="../js/jquery.stellar.min.js"></script>
-     <script src="../js/wow.min.js"></script>
-     <script src="../js/smoothscroll.js"></script>
-     <script src="../js/owl.carousel.min.js"></script>
-     <script src="../js/custom.js"></script>
+  <!-- SCRIPTS -->
+  <script src="../js/jquery.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery.sticky.js"></script>
+  <script src="../js/jquery.stellar.min.js"></script>
+  <script src="../js/wow.min.js"></script>
+  <script src="../js/smoothscroll.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/custom.js"></script>
 
 </body>
 </html>
