1250f0834b68c3df94843baa1d3f24433c64a9b9
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-10-31 18:53:02 +0530

    Minor edit
    Changed the timeout value in msgcuntscript.js to 1 minute

--- user/dispenser/msgcountscript.js
+++ user/dispenser/msgcountscript.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 			success:function(data)
 			{
 				$('#msgCount').html(data);
-            },
+			},
 			complete: function() {
 				// Schedule the next request when the current one's complete
-				setTimeout(load_data("msgcount"), 2000); //the load_data function will execute every two seconds
+				setTimeout(load_data("msgcount"), 60000); //the load_data function will execute every one minute
 			}
 		});
     }
 
-    load_data("msgcount");
+	load_data("msgcount");
 });
