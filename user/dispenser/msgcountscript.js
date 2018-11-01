914df06d4700fa2f2b0d6d93a88fc1cd5a8387a4
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-11-01 21:58:33 +0530

    Minor update
    Removed the set timeout in msgcountscript.js

--- user/dispenser/msgcountscript.js
+++ user/dispenser/msgcountscript.js
@@ -9,10 +9,6 @@ $(document).ready(function(){
 			success:function(data)
 			{
 				$('#msgCount').html(data);
-			},
-			complete: function() {
-				// Schedule the next request when the current one's complete
-				setTimeout(load_data("msgcount"), 60000); //the load_data function will execute every one minute
 			}
 		});
     }
