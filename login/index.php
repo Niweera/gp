b32577905c945a297d70f218011e60564b5225d3
snehaudi <39775041+snehaudi@users.noreply.github.com>
2018-11-25 17:53:23 +0530

    Update index.php

--- login/index.php
+++ login/index.php
@@ -133,8 +133,6 @@
                                     <li><a href="../services/opd.php">OPD Services</a></li>
                                 </ul>
                         </li>
-                         
-                        <li><a href="../contact" class="smoothScroll">Contact Details</a></li>
 
                         <li class="nav-item">
                             <?php
@@ -190,8 +188,6 @@
                     <div class="col-md-4 col-sm-4">
                          <div class="footer-thumb"> 
                               <h4 class="wow fadeInUp" data-wow-delay="0.4s">Contact Info</h4>
-                              <p>Fusce at libero iaculis, venenatis augue quis, pharetra lorem. Curabitur ut dolor eu elit consequat ultricies.</p>
-
                               <div class="contact-info">
                                    <p><i class="fa fa-phone"></i> 010-070-0170</p>
                                    <p><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></p>
