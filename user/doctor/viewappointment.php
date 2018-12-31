946656b0e5b247c80dae173f28646ceb0780df41
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-12-31 10:59:06 +0530

    Update doctor

--- user/doctor/viewappointment.php
+++ user/doctor/viewappointment.php
@@ -42,7 +42,7 @@
 </head>
 <body>
 <!--Header navigation bar for the website-->
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #212529;">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0097a7;box-shadow: 0px 0px 12px #828282;">
     <a class="navbar-brand" href="../../">Divisional Hospital, Bentota</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -107,7 +107,7 @@
         </div>
         <div class="form-group row mb-5">
             <div class="col-sm-5"></div>
-            <input type="button" value="Go Back" class="col-md-2 text-center btn btn-primary btn" onclick="history.go(-1);">
+            <input type="button" value="Go Back" class="col-md-2 text-center btn btn-info btn" onclick="history.go(-1);">
             <div class="col-sm-5"></div>
         </div>
     </div>    
