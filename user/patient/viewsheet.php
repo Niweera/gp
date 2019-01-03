39745caddc638702a985155d1134144da25f8d5c
snehaudi <39775041+snehaudi@users.noreply.github.com>
2019-01-03 14:17:03 +0530

    Update viewsheet.php

--- user/patient/viewsheet.php
+++ user/patient/viewsheet.php
@@ -48,7 +48,7 @@ if (null !==(filter_input(INPUT_POST, 'submit'))){
 <body>
 
     <div id="printthis" class="container mt-5 mb-5">
-        <div class="container border pt-4 bg-light rounded">
+        <div class="container border bg-light rounded">
             <div class="row">
                 <div class="col-md-12 bg-light border">
                     <h1 class="text-center">Divisional Hospital, Bentota</h1>
