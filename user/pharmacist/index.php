b2d90695d80649c9f17151f0065d3447ffc8de38
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-01-04 11:59:32 +0530

    Update pharmacist favicon

--- user/pharmacist/index.php
+++ user/pharmacist/index.php
@@ -21,7 +21,7 @@
 
     <title>Welcome <?php echo $_SESSION['userid'];?></title>
 
-    <link rel="shortcut icon" type="image/png" href="https://www.niwder.me/tvdb/logo.jpg"/>
+    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="../../images/favicon.ico"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
